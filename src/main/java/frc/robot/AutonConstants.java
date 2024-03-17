eeb376f581923f1c1703f94a67c5003ee90da68c
Logan Fick <63548711+WeasalCrafter@users.noreply.github.com>
2024-03-16 21:18:13 -0400

    dfgvhbn

--- src/main/java/frc/robot/AutonConstants.java
+++ src/main/java/frc/robot/AutonConstants.java
@@ -4,7 +4,7 @@ public class AutonConstants {
     public static final class HighShot {
         public static final double DISTANCE_TO_NOTE_METERS = Constants.loganInches(40);
         public static final double DISTANCE_BETWEEN_NOTES_METERS = Constants.loganInches(59);
-        public static final double HORIZONTAL_OFFSET_SIDE = Constants.loganInches(1); //TODO
+        public static final double HORIZONTAL_OFFSET_SIDE = Constants.loganInches(26); //TODO
         public static final double SIDE_ANGLE_DEGREES = 30; //TODO
         public static final double SHOOT_TIME = 1; 
         public static final double OUTTAKE_TIME = 0.075;
